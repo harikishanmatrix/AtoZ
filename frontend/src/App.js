2061bf85b09052b012a55182292fb0982239a67d
harikishanmatrix <harikishan14087@gmail.com>
2022-11-23 14:04:55 +0530

    Props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import Hotels from './component';
 
 function Food(){
+  const hotels = [
+    {
+      id : 1,
+      name : 'Rajugaribiryani',
+      location : 'Hyderabad'
+      },
+      {
+        id : 2,
+        name : 'nest',
+        location : 'Benguluru'
+      }
+  ]
+ 
+
   return(
-    <div>
-      <Hotels/>
-    </div>
+<Hotels hotel={hotels}/>
+ 
   )
-}
+    }
 
 export default Food;
--- frontend/src/component.js
+++ frontend/src/component.js
@@ -1,7 +1,16 @@
-function Hotels(){
+import './hoels.css'
+function Hotels(props){
+    const hoteldetails = props.hotel.map((hotel)=>{
+        return(
+    <div key = {hotel.id} className="hotel">
+         <h1>{hotel.name}</h1>
+         <h1>{hotel.location}</h1>
+    </div>
+        )
+      })
     return(
         <div>
-            <h1>Rajugaribiryani</h1>
+            <h1>{hoteldetails}</h1>
         </div>
     )
 }
